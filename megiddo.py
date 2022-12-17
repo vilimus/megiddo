b9796d8de97fc93d8f1bb1adf464678789d46b79
vilimus <35790256+vilimus@users.noreply.github.com>
2022-12-17 03:50:31 -0500

    Update megiddo.py

--- megiddo.py
+++ megiddo.py
@@ -13,7 +13,7 @@ class Megiddo():
   def get(self, i: int, j: int):
     return self.board[i%6, j%6]
 
-  def place(self, i: int, j: int, x: int):
+  def set(self, i: int, j: int, x: int):
     self.board[i%6, j%6] = x
 
   def move(self, i: int, j: int):
@@ -21,16 +21,16 @@ class Megiddo():
     if self.get(i,j) != 0:
       return
     
-    self.place(i, j, (-1)**(self.turn%2))
+    self.set(i, j, (-1)**(self.turn%2))
     self.turn += 1
 
     # capture
     x = self.get(i,j)
     for di,dj in [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]:
-      a, b, c = [self.get(i+k*di,j+k*dj) for k in [1,2,3]]
-      if a==-x and b==-x and c==x:
-        self.place(i+di,j+dj,x)
-        self.place(i+2*di,j+2*dj,x)
+      a,b,c = [self.get(i+k*di,j+k*dj) for k in range(1,4)]
+      if a==b==-x and c==x:
+        self.set(i+di,j+dj,x)
+        self.set(i+2*di,j+2*dj,x)
 
     # win condition
     for i in range(6):
